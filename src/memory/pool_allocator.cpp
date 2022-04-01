6f80bf13fc8cade7c30e74f222b819431d6dcf4c
Abel Kalavadakken <abelkalavadakken@github.com>
2022-04-01 12:00:00 +0530

    Add pool_allocator.cpp

---
+++ src/memory/pool_allocator.cpp
@@ -0,0 +1,8 @@
+// Template class implementation is in pool_allocator.tpp
+// This file is kept for potential non-template utility functions
+
+#include "nanotrader/memory/pool_allocator.hpp"
+
+namespace nanotrader {
+// Template implementations are in the .tpp file
+} // namespace nanotrader
